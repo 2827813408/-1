94a39b162a32bfb41f0cf4c48e2aeaad32daa34c
林涛涛 <2827813408@qq.com>
2021-07-16 03:15:59 +0800

    1.C
    计算函数

---
+++ 1.C
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+#include<stdio.h>
+int main()
+{
+
+	int tao1 = 0;
+	int tao2 = 0;
+	int sum = 0;
+scanf("%d %d", &tao1, &tao2);
+	sum = tao1 + tao2;
+	printf("%d\n", sum);
+	return 0;
+}
